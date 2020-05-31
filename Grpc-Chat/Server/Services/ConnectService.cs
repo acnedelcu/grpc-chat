e9d32a987827e50a3840e12d2d19a8e1adb4eaa6
Cosmin <andreicosminnedelcu@gmail.com>
2020-05-31 18:49:41 +0300

    Created service to pass username to Server.

---
+++ Grpc-Chat/Server/Services/ConnectService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Server.Protos;
+
+namespace Server
+{
+    public class ConnectService:Connect.ConnectBase
+    {
+        public ConnectService()
+        {
+
+        }
+        public override Task<Confirmation> SendUsername(ConnectedRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("User "+request.Username+" has connected!");
+
+            Confirmation confirmationMessage = new Confirmation();
+            string messageToSend= "You have sucessfully connected to the server!";
+            confirmationMessage.ConnectedConfirmation = messageToSend;
+            return Task.FromResult(confirmationMessage);
+        }
+    }
+}
