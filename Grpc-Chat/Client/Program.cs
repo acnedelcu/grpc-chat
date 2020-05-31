5c8e8af0383d5eb75ef42e7aa519d74137a92a28
Cosmin <andreicosminnedelcu@gmail.com>
2020-05-31 18:54:32 +0300

    Added functionality to client's Main function.

--- Grpc-Chat/Client/Program.cs
+++ Grpc-Chat/Client/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Server.Protos;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Connect.ConnectClient(channel);
+
+            Console.WriteLine("Please type in your name:");
+            string name = Console.ReadLine();
+
+            var username = new ConnectedRequest { Username = name };
+            var reply = await client.SendUsernameAsync(username);
+            Console.WriteLine(reply.ConnectedConfirmation);
             Console.ReadLine();
+
         }
     }
 }
